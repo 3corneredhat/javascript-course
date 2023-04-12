b32ae13481b104e090895c437ade2af8f5145094
3corneredhat <alancabrera10@gmail.com>
2023-04-12 10:36:36 -0500

    feat: explored variables and conventions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,24 @@
 let js = "amazing";
-if (js === "amazing") {
-  alert("js is fun");
-}
+
 console.log(23 + 2 + 5);
+
+console.log("Jonas");
+console.log(23);
+
+let firstName = "Mathilda";
+let lastName = "Professional";
+
+console.log(firstName);
+console.log(firstName);
+
+// Name is a reserved keyword but is still legal.
+// Objects start with capitals.
+// Constants should be uppercase.
+// Variable names should be descriptive.
+
+let myFirstJob = "Programmer";
+let myCurrentJob = "Teacher";
+
+// These are less descriptive
+let job1 = "programmer";
+let job2 = "teacher";
