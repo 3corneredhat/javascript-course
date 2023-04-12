1011b455fa32073e9caad019ccc8a5a13f18f7eb
3corneredhat <alancabrera10@gmail.com>
2023-04-12 13:53:41 -0500

    feat: explored types in js

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -22,3 +22,25 @@ let myCurrentJob = "Teacher";
 // These are less descriptive
 let job1 = "programmer";
 let job2 = "teacher";
+
+// There are 7 Primitive Data Types
+//
+// 1. number
+// 2. string
+// 3. boolean
+// 4. undefined - value taken by a variable that is not yet defined ('empty value')
+// 5. null - also an empty value
+// 6. Symbol - value that is unique and can't be changed
+// 7. BigInt - larger integers than the Number type can hold
+//
+// Dynamic typing is used where we do not have to manually define the data type of
+// the value stored in a variable.
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
